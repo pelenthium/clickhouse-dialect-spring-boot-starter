9cf6767883602adc1eb9c6b6ac2e019edcf2d860
pelenthium <pelenthium@gmail.com>
2023-07-19 19:02:09 +0800

    #8 Lock not supported in Clickhouse exception - do not throw exception, use noop LockCause

--- src/main/java/com/github/pelenthium/spring/dialect/ClickhouseDialect.java
+++ src/main/java/com/github/pelenthium/spring/dialect/ClickhouseDialect.java
@@ -3,40 +3,56 @@ package com.github.pelenthium.spring.dialect;
 import org.springframework.data.relational.core.dialect.AnsiDialect;
 import org.springframework.data.relational.core.dialect.LimitClause;
 import org.springframework.data.relational.core.dialect.LockClause;
+import org.springframework.data.relational.core.sql.LockOptions;
 
 public class ClickhouseDialect extends AnsiDialect {
 
     static final ClickhouseDialect INSTANCE = new ClickhouseDialect();
 
-    private ClickhouseDialect() {}
+    static final LimitClause LIMIT_INSTANCE = new LimitClause() {
+        @Override
+        public String getLimit(long limit) {
+            return String.format("LIMIT %d", limit);
+        }
+
+        @Override
+        public String getOffset(long offset) {
+            return getLimitOffset(Long.MAX_VALUE, offset);
+        }
+
+        @Override
+        public String getLimitOffset(long limit, long offset) {
+            return String.format("LIMIT %d OFFSET %d", limit, offset);
+        }
+
+        @Override
+        public Position getClausePosition() {
+            return Position.AFTER_ORDER_BY;
+        }
+    };
+
+    static final LockClause LOCK_INSTANCE = new LockClause() {
+        @Override
+        public String getLock(LockOptions lockOptions) {
+            return "";
+        }
+
+        @Override
+        public LockClause.Position getClausePosition() {
+            return null;
+        }
+    };
+
+    private ClickhouseDialect() {
+    }
 
     @Override
     public LockClause lock() {
-        throw new UnsupportedOperationException("Lock not supported in Clickhouse");
+        return LOCK_INSTANCE;
     }
 
     @Override
     public LimitClause limit() {
-        return new LimitClause() {
-            @Override
-            public String getLimit(long limit) {
-                return String.format("LIMIT %d", limit);
-            }
-
-            @Override
-            public String getOffset(long offset) {
-                return getLimitOffset(Long.MAX_VALUE, offset);
-            }
-
-            @Override
-            public String getLimitOffset(long limit, long offset) {
-                return String.format("LIMIT %d OFFSET %d", limit, offset);
-            }
-
-            @Override
-            public Position getClausePosition() {
-                return Position.AFTER_ORDER_BY;
-            }
-        };
+        return LIMIT_INSTANCE;
     }
 }
